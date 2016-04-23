77febbf1662ca109150ec284d13f2815b26fecd1
Ezard <ezard.ben@gmail.com>
2016-04-23 11:52:57 +0100

    Basic setup

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({
+    defaultLayout: __dirname + '/views/layouts/main'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(80, function () {
+});
